ac1a87ffe6d1f0df8bf45776e40b8dbbfe7d0aa9
张果 <237625092@qq.com>
2018-05-03 15:17:48 +0800

    demo

--- client/app.js
+++ client/app.js
@@ -1,5 +1,9 @@
-App({
-  onLaunch: function () {
+//app.js
+var qcloud = require('./vendor/wafer2-client-sdk/index')
+var config = require('./config')
 
-  }
+App({
+    onLaunch: function () {
+        qcloud.setLoginUrl(config.service.loginUrl)
+    }
 })
