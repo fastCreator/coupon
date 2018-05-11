55398e828fca4cfe32504bba16181241b9225d15
张果 <237625092@qq.com>
2018-05-11 09:03:24 +0800

    sesssion

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,11 +1,17 @@
 const app = getApp()
 Page({
   data: {},
-  onLoad: function () { },
-  onReady: function () { },
-  toSearch:function(){
+  onLoad() { },
+  onReady() { },
+  toSearch() {
     wx.navigateTo({
       url: '../search/index'
     })
+  },
+  theme(e) {
+    wx.navigateTo({
+      url: '../list/index'
+    })
+    console.log(e.target.dataset.type)
   }
 })
--- server/controllers/tbk.js
+++ server/controllers/tbk.js
@@ -3,6 +3,7 @@ var ApiClient = require('../lib/api/topClient.js').TopClient
 // var tmcClient = require('../lib/tmc/tmcClient.js').TmcClient
 
 const client = new ApiClient({
+    'session': '6101d069b05c54189b247481c2d0a3e893f3b17105c00be87491795',
     'appkey': '24884330',
     'appsecret': 'bac78cb1d551fbcfd517b8e10d6c310d',
     'REST_URL': 'http://gw.api.taobao.com/router/rest'
