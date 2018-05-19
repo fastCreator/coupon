e9147d3e2e7377a4983da397cf1c504fdf90a97c
张果 <237625092@qq.com>
2018-05-19 20:37:27 +0800

    '.'

--- mint/src/utils/utils.js
+++ mint/src/utils/utils.js
@@ -23,6 +23,8 @@ export default {
   },
   is_weixn () {
     var ua = navigator.userAgent.toLowerCase()
+    alert(ua)
+    alert(ua.match(/MicroMessenger/i) === 'micromessenger')
     if (ua.match(/MicroMessenger/i) === 'micromessenger') {
       return true
     } else {
