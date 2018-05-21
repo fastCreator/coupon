8a673615a48a6897b2235bab13eb20b1f73bb9f2
Your Name <you@example.com>
2018-05-21 17:49:13 +0800

    '.'

--- server/controllers/imgUrls.js
+++ server/controllers/imgUrls.js
@@ -1,37 +1,37 @@
 const data = [
-  {
-    img: "http://alp.alicdn.com/1476310476060.png",
-    clip: {
-      title: '大咖推荐折扣商品',
-      kl: '￥ZeV80udd5Ki￥',
-      dlj: 'https://s.click.taobao.com/UQ5zvQw'
+    {
+        img: 'http://gtms04.alicdn.com/tps/i4/TB1zBf8LFXXXXaAaXXXQO4D5VXX-440-180.jpg',
+        clip: {
+            title: '天猫精选-品牌惠聚',
+            kl: '￥yVAt0FqlHZx￥',
+            dlj: 'https://s.click.taobao.com/EauDcQw'
+        }
+    },
+    {
+        img: 'https://img.alicdn.com/tfs/TB101ZQdgoQMeJjy0FnXXb8gFXa-440-180.jpg',
+        clip: {
+            title: '天猫必抢',
+            kl: '￥XMqv0Fqm74x￥',
+            dlj: 'https://s.click.taobao.com/HWYDcQw'
+        }
+    },
+    {
+        img: 'https://gw.alicdn.com/tfs/TB1KE6rkwoQMeJjy1XaXXcSsFXa-440-180.jpg',
+        clip: {
+            title: '精选淘宝/天猫最热门优惠券',
+            kl: '￥QzLp0FqnZkS￥',
+            dlj: 'https://s.click.taobao.com/uv7DcQw'
+        }
+    },
+    {
+        img: 'https://gw.alicdn.com/tfs/TB1EfElgWagSKJjy0FgXXcRqFXa-440-180.jpg',
+        clip: {
+            title: '爱淘宝',
+            kl: '￥RXge0FqnGUK￥',
+            dlj: 'https://s.click.taobao.com/JnuCcQw'
+        }
     }
-  },
-  {
-    img: "https://img.alicdn.com/tfs/TB14dbgbuOSBuNjy0FdXXbDnVXa-440-180.png",
-    clip: {
-      title: '淘宝新人福利社',
-      kl: '￥cpxX0udXJSk￥',
-      dlj: 'https://s.click.taobao.com/sDIzvQw'
-    }
-  },
-  {
-    img: "https://img.alicdn.com/tfs/TB1xL0domBYBeNjy0FeXXbnmFXa-440-180.jpg",
-    clip: {
-      title: '天猫新人福利社',
-      kl: '￥NpsW0udXwEs￥',
-      dlj: 'https://s.click.taobao.com/fpDzvQw'
-    }
-  },
-  {
-    img: "https://img.alicdn.com/tfs/TB11PbrpxSYBuNjSspjXXX73VXa-611-253.png",
-    clip: {
-      title: '支付宝新人福利社',
-      kl: '￥mn8M0ud2TCB￥',
-      dlj: 'https://s.click.taobao.com/dW9zvQw'
-    }
-  }
 ]
 module.exports = async (ctx, next) => {
-  ctx.body = data 
+    ctx.body = data
 }
