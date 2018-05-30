62c23d9afa149e7691e406288039561705c5496c
Your Name <you@example.com>
2018-05-30 23:04:31 +0800

    '.'

--- server/controllers/imgUrls.js
+++ server/controllers/imgUrls.js
@@ -1,5 +1,5 @@
 const data = [
-    {'imgurl': 'http://img3.imgtn.bdimg.com/it/u=3234383353,1941559374&fm=200&gp=0.jpg', 'type': '0', 'keyword': '水果'}
+    {'imgurl': 'http://img4.imgtn.bdimg.com/it/u=4176539679,2071232867&fm=200&gp=0.jpg', 'type': '0', 'keyword': '水果'}
 ]
 module.exports = async (ctx, next) => {
     ctx.body = data
