dbb381846cb90e302314e67bf3ae810f70d60132
Your Name <you@example.com>
2018-05-30 22:57:20 +0800

    '.'

--- server/controllers/imgUrls.js
+++ server/controllers/imgUrls.js
@@ -1,5 +1,5 @@
 const data = [
-    {'imgurl': 'https://img.alicdn.com/tfscom/i4/2128793927/TB24Hw_qHSYBuNjSspiXXXNzpXa_!!2128793927-0-item_pic.jpg_310x310.jpg', 'type': '0', 'keyword': '水果'}
+    {'imgurl': 'http://img4.imgtn.bdimg.com/it/u=4176539679,2071232867&fm=200&gp=0.jpg', 'type': '0', 'keyword': '水果'}
 ]
 module.exports = async (ctx, next) => {
     ctx.body = data
